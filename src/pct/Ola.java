e76ffe9931fdf729cd20a4628a87665d95c9f94f
Delfino <Delfino@192.168.1.8>
2021-12-29 13:45:49 -0300

    commit tonho dia 06 de agosto.

---
+++ src/pct/Ola.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pct;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Delfino
+ */
+public class Ola {
+    public static void main(String[] args) {
+        
+        JOptionPane.showMessageDialog(null, "BOA TARDE Á TODOS");
+        
+    }
+    
+}
